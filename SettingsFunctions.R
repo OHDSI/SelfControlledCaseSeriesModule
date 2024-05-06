fea741ebf71c7c1af72d8459d85c8494850b4dfe
Anthony Sena <asena5@its.jnj.com>
2024-05-06 13:42:03 -0400

    Use pre-release of Cyclops

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -12,7 +12,7 @@ creatSelfControlledCaseSeriesModuleSpecifications <- function(sccsAnalysisList,
   }
 
   specifications <- list(module = "SelfControlledCaseSeriesModule",
-                         version = "0.4.2-4",
+                         version = "0.4.2-5",
                          remoteRepo = "github.com",
                          remoteUsername = "ohdsi",
                          settings = analysis)
