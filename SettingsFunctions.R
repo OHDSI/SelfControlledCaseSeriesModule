1f499155fbddccfa61bab9114e2bdb0d770b8c62
Jamie Gilbert <JGilber2@its.jnj.com>
2024-05-29 13:01:59 -0400

    Pre-release for attrition fix (#16)
    * Pre-release for attrition fix
    
    * Update SettingsFunctions.R
    
    * Update MetaData.json
    
    * Update SettingsFunctions.R
    
    * Update SettingsFunctions.R

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -12,7 +12,7 @@ creatSelfControlledCaseSeriesModuleSpecifications <- function(sccsAnalysisList,
   }
 
   specifications <- list(module = "SelfControlledCaseSeriesModule",
-                         version = "0.4.2-5",
+                         version = "0.5.0-1",
                          remoteRepo = "github.com",
                          remoteUsername = "ohdsi",
                          settings = analysis)
