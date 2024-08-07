5a1b350f1d48814404a1c477983e8f51c9c7b6c9
Jamie Gilbert <JGilber2@its.jnj.com>
2024-08-07 11:45:53 -0700

    Update SettingsFunctions.R

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -12,7 +12,7 @@ creatSelfControlledCaseSeriesModuleSpecifications <- function(sccsAnalysisList,
   }
 
   specifications <- list(module = "SelfControlledCaseSeriesModule",
-                         version = "0.5.1",
+                         version = "0.5.3",
                          remoteRepo = "github.com",
                          remoteUsername = "ohdsi",
                          settings = analysis)
